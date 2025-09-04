0ae1c4ad0d89a9e7e474078ddf1a58fa1201e05b
Charles Liao <charles.liao@oocl.com>
2025-09-04 18:57:28 +0800

    refactor: streamline result formatting by introducing a dedicated method for frequency output

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,34 +9,25 @@ public class WordFrequencyGame {
     public static final String ANY_SPACE_SEPARATOR = "\\s+";
 
     public String getResult(String inputStr) {
-
-
         if (inputStr.split(ANY_SPACE_SEPARATOR).length == 1) {
             return inputStr + " 1";
 
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
                 String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
-
                 List<Input> frequencies = countFrequency(words);
-
                 frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : frequencies) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
+                return formatFrequencies(frequencies);
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
+    }
+    private static String formatFrequencies(List<Input> frequencies) {
+        return frequencies.stream()
+                .map(w -> w.getValue() + " " + w.getWordCount())
+                .collect(java.util.stream.Collectors.joining("\n"));
     }
 
     private List<Input> countFrequency(String[] words) {
@@ -47,7 +38,6 @@ public class WordFrequencyGame {
                 .map(entry -> new Input(entry.getKey(), entry.getValue().size()))
                 .collect(java.util.stream.Collectors.toList());
     }
-
 
     private static Map<String, List<String>> groupSameWords(String[] words) {
         return java.util.Arrays.stream(words)
